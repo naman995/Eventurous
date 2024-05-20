1be6e0f5f26c69b438488a9f0b7bad4cf10843d6
naman995 <rajputnaman823@gmail.com>
2024-05-20 21:14:58 +0530

    refactor: Update Footer component to use className instead of class attribute

--- eventurous/src/app/components/Footer.js
+++ eventurous/src/app/components/Footer.js
@@ -59,8 +59,8 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+          <div className="ml-8 md:ml-0">
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white ">
               Legal
             </h2>
             <ul class="text-gray-500 dark:text-gray-400 font-medium">
